a555eec0280992852c3e528d70b309cce395e7c0
AngryRaccoon <03sidcrafter@gmail.com>
2023-11-22 01:45:28 +0300

    Add Karamba from Bart Simpson

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Добавлено в ветке dev");
         System.out.println("something change");
+        System.out.println("Karamba");
     }
 }
