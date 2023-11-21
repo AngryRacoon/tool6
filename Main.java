908fea1b5f35f0bf537a536759558ccc12116abd
AngryRaccoon <03sidcrafter@gmail.com>
2023-11-22 01:17:30 +0300

    Add big changes in Main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Это добавлено в ветке main");
         System.out.println("add some changes");
+        System.out.println("add big changes");
     }
 }
